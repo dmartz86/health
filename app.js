7f15ba2f24dd002669d13e55ed0489ef19b9b983
Daniel Martinez <dmartz86@gmail.com>
2016-04-20 18:38:28 -0500

    Add paginator

--- app.js
+++ app.js
@@ -5,13 +5,18 @@
   function PatientsController($scope, $http, $timeout) {
     var API = 'https://demo3417391.mockable.io/';
     var statuses = [];
+    $scope.page = {
+      size: 10, 
+      current: 1,
+      previous: 0,
+      next: 0,
+      count: 0
+    };
 
     function init() {
       $http.get(API + 'patient_status')
         .then(assignStatus)
-        .then(function () {
-          return $http.get(API + 'patients');
-        })
+        .then(retrievePatients)
         .then(setPatients)
         .catch(console.error);
     }
@@ -19,8 +24,15 @@
     function assignStatus(res) {
       statuses = res.data.results;
     }
+    
+    function retrievePatients() {
+      return $http.get(API + 'patients?page=' + $scope.page.current);
+    }
 
     function setPatients(res) {
+      $scope.page.count = res.data['count'];
+      $scope.page.next = res.data['next_page'];
+      $scope.page.previous = res.data['previous_page'];
       $scope.patients = res.data.results.map(function (p) {
         var fullName = p.name.split(' ');
         p.fname = fullName[0];
@@ -36,7 +48,7 @@
       if (!found.length) { return ''; }
       return found[0].status
     }
-    
+
     function sortBy(key) {
       if ($scope.sort === key) {
         $scope.inverse = !$scope.inverse;
@@ -44,14 +56,22 @@
         $scope.sort = key;
       }
     }
+    
+    function passThePage(page) {
+      if (!page) {return;}
+      $scope.page.current = page;
+      retrievePatients()
+      .then(setPatients)
+      .catch(console.error);
+    }
 
     $scope.search = '';
     $scope.sort = 'id';
-    
     $scope.inverse = false;
     $scope.patients = [];
     $scope.sortBy = sortBy;
     $scope.getStatus = getStatus;
+    $scope.passThePage = passThePage;
     $timeout(init, 0);
   }
 })();
